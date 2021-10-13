fb5ecd74fab6d66fd00cea735b4ef94d57b4cd33
lzh2740128806 <2740128806@qq.com>
2021-10-13 14:39:07 +0800

    Init project

---
+++ js/data.js
@@ -0,0 +1,140 @@
+
+var docs = [
+  {
+      "title": "Can I Use",
+      "url": "http://caniuse.com/"
+  },
+  {
+      "title": "Web Events",
+      "url": "https://developer.mozilla.org/zh-CN/docs/Web/Events"
+  },
+  {
+      "title": "html Reference",
+      "url": "https://developer.mozilla.org/zh-CN/docs/Web/HTML/Reference"
+  },
+  {
+      "title": "css Reference",
+      "url": "https://developer.mozilla.org/zh-CN/docs/Web/CSS/Reference"
+  },
+  {
+      "title": "javascript Reference",
+      "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference"
+  },
+  {
+      "title": "Web API",
+      "url": "https://developer.mozilla.org/zh-CN/docs/Web/API"
+  },
+  {
+      "title": "Canvas API",
+      "url": "https://developer.mozilla.org/zh-CN/docs/Web/API/WebGL_API"
+  },
+  {
+      "title": "Canvas API",
+      "url": "https://developer.mozilla.org/zh-CN/docs/Web/API/Canvas_API/Tutorial"
+  },
+  {
+      "title": "Web SVG",
+      "url": "https://developer.mozilla.org/zh-CN/docs/Web/SVG/Element/svg"
+  },
+  {
+      "title": "node",
+      "url": "http://nodejs.cn/"
+  },
+  {
+      "title": "webpack",
+      "url": "https://webpack.js.org/"
+  },
+  {
+      "title": "learn node.js",
+      "url": "http://nqdeng.github.io/7-days-nodejs/"
+  },
+  {
+      "title": "three.js",
+      "url": "http://techbrood.com/threejs/docs/"
+  },
+  {
+      "title": "Postcss",
+      "url": "https://github.com/postcss/postcss"
+  },
+  {
+      "title": "阮一峰 ES6",
+      "url": "http://es6.ruanyifeng.com/"
+  },
+  {
+      "title": "阮一峰 JavaScript",
+      "url": "https://wangdoc.com/javascript/"
+  },
+  {
+      "title": "Lodash.js",
+      "url": "http://lodashjs.com/"
+  },
+  {
+      "title": "velocity.js",
+      "url": "http://www.velocityjs.org/"
+  },
+  {
+      "title": "text-spinners",
+      "url": "https://github.com/maxbeier/text-spinners"
+  },
+  {
+      "title": "loaders.css",
+      "url": "https://connoratherton.com/loaders"
+  }
+]
+
+var resources = [
+  {
+    "title": "Web前端导航",
+    "url": "http://www.alloyteam.com/nav/"
+  },
+  {
+      "title": "Mozilla 开发者网络",
+      "url": "https://developer.mozilla.org/zh-CN/"
+  },
+  {
+      "title": "w3ctech",
+      "url": "https://www.w3ctech.com/"
+  },
+  {
+      "title": "易百",
+      "url": "https://www.yiibai.com/"
+  },
+  {
+      "title": "UI中国",
+      "url": "http://www.ui.cn/"
+  },
+];
+
+var blogs = [
+  {
+    "title": "阮一峰",
+    "url": "http://www.ruanyifeng.com/"
+  },
+  {
+      "title": "张鑫旭",
+      "url": "http://www.zhangxinxu.com/"
+  },
+  {
+      "title": "廖雪峰",
+      "url": "https://www.liaoxuefeng.com/"
+  },
+  {
+      "title": "腾讯Web前端团队",
+      "url": "http://alloyteam.github.io/"
+  },
+]
+
+window.cates = [
+  {
+    title: '资源',
+    resources: resources,
+  },
+  {
+    title: '博客',
+    resources: blogs,
+  },
+  {
+    title: '文档',
+    resources: docs,
+  },
+];
